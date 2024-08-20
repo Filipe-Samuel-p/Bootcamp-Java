868d1748875a97029e00dd36b7a3226f68ab0ed7
Filipe Samuel <felipepires.p125@gmail.com>
2024-08-19 23:24:02 -0300

    fix: Introduction dataBase

---
+++ src/DataBase_JDBC/db/Program.java
@@ -0,0 +1,10 @@
+package DataBase_JDBC.db;
+
+import java.sql.Connection;
+
+public class Program {
+    public static void main(String[] args) {
+        Connection conn = DB.getConnection();
+        DB.closeConnection();
+    }
+}
